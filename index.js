e0e994337d50bcfdb9670bc171bd44fd8f92a663
Owen Buckley <owenbuckley13@gmail.com>
2024-11-18 18:01:05 -0500

    add require.resolve fallback check

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const DEPS = ['lit', '@types/trusted-types'];
+import { createRequire } from 'module';
 
+const DEPS = ['lit', '@types/trusted-types'];
 
 for(const dep of DEPS) {
   try {
     const resolved = import.meta.resolve(dep);
 
     console.log(`✅ SUCCESS: resolved ${dep} at location => `, resolved);
-  } catch(e) {
-    console.error(`🚨 ERROR: could not resolve ${dep}`, e);
+  } catch(err) {
+    console.log(`⚠️ WARNING: could not resolve ${dep}, falling back to \`require.resolve\``, err);
+
+    try {
+      const require = createRequire(import.meta.url);
+      const requireResolved = require.resolve(dep);
+    } catch (e) {
+      console.error(`🚨 ERROR: could not resolve ${dep}`, e);
+    }
   }
 }
