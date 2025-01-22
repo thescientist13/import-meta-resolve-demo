06ece53a2b149c089e6d219354143b0c49a8b92f
Owen Buckley <owenbuckley13@gmail.com>
2025-01-21 21:16:23 -0500

    pull dependencies from package.json

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+import fs from 'node:fs';
 import { createRequire, isBuiltin } from 'node:module';
 
-const DEPS = ['lit', '@types/trusted-types', 'process', '@libsql/core', '@types/ws'];
+const { dependencies = {} } = JSON.parse(fs.readFileSync(new URL('./package.json', import.meta.url), 'utf-8'));
 
-for(const dep of DEPS) {
+for(const dependency of Object.keys(dependencies)) {
   try {
-    const resolved = import.meta.resolve(dep);
+    const resolved = import.meta.resolve(dependency);
     const isNodeBuiltIn = isBuiltin(resolved);
     const marker = resolved && isNodeBuiltIn ? '⚠️ WARNING' : '✅ SUCCESS';
 
-    console.log(`\n ${marker}: resolved ${dep} at location => ${resolved} ({ isNodeBuiltIn: ${isNodeBuiltIn} })`);
+    console.log(`\n ${marker}: resolved ${dependency} at location => ${resolved} ({ isNodeBuiltIn: ${isNodeBuiltIn} })`);
   } catch(err) {
-    console.log(`\n ⚠️ WARNING: could not resolve ${dep} using \`import.meta.url\`' falling back to \`require.resolve\` - `, err);
+    console.log(`\n ⚠️ WARNING: could not resolve ${dependency} using \`import.meta.url\`' falling back to \`require.resolve\` - `, err);
 
     try {
       const require = createRequire(import.meta.url);
 
-      require.resolve(dep);
-      console.log(`\n ✅ SUCCESS: was able to resolve ${dep} using \`require.resolve\``);
+      require.resolve(dependency);
+      console.log(`\n ✅ SUCCESS: was able to resolve ${dependency} using \`require.resolve\``);
     } catch (e) {
-      console.error(`\n 🚨 ERROR: could not resolve ${dep} using \`require.resolve\` - `, e);
+      console.error(`\n 🚨 ERROR: could not resolve ${dependency} using \`require.resolve\` - `, e);
     }
   }
   console.log('-------------------------------------------------------------------------')
